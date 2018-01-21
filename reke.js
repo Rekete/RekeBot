ec61a915d2bb746bb1a481c3bba4249733228e16
Rekete <35660663+Rekete@users.noreply.github.com>
2018-01-21 18:38:11 +0100

    Update reke.js

--- reke.js
+++ reke.js
@@ -1,13 +1,11 @@
+import * as Discord from 'discord.js';
+import * as core from './config/core';
 import * as commands from './commands/commands';
 
-const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
 client.on('message', (message) => {
     commands.execute(client, message);
 });
-// 
-client.login(process.env.BOT_TOKEN);
+
+core.login(client);
