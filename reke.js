49b4c071c21798746c0c657be056f32a1b07df70
Rekete <35660663+Rekete@users.noreply.github.com>
2018-01-22 14:26:52 +0100

    Update reke.js

--- reke.js
+++ reke.js
@@ -136,7 +136,22 @@ client.on("message", async message => {
     } catch (err) {
         console.log(err);
     }}
+    
+    if(command === "ripple") {
+        
+    try {
+        if (args.length == 0) return await message.reply("Por favor, especifica un nombre de usuario de ripple! D:");
 
+        const username = args.join("%20");
+
+        const channel = await client.channels.get(message.channel.id);
+        const hex = randomColor().replace("#", "");
+        return message.reply(`http://sig.ripple.moe/sig.php?colour=hex${hex}&uname=${username}&mode=3&pp=0&countryrank`);
+
+    } catch (err) {
+        console.log(err);
+    }}
+    
 });
 // 
 client.login(process.env.BOT_TOKEN);
