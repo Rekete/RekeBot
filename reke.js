35714a2e682846f56b8ffdaee54ce0544186ee5a
Rekete <35660663+Rekete@users.noreply.github.com>
2018-01-22 12:31:44 +0100

    Update reke.js

--- reke.js
+++ reke.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require("./config/config.json");
 
 client.on('ready', () => {
     console.log('Estoy listooo yujuuu!');
