80c49fa7b67c8f6b797d711aac8e8985ac981516
Rekete <35660663+Rekete@users.noreply.github.com>
2018-01-22 13:51:56 +0100

    Update reke.js

--- reke.js
+++ reke.js
@@ -119,6 +119,24 @@ client.on("message", async message => {
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
+    
+    if(command === "osu") {
+        import { randomColor } from 'randomcolor';
+
+export async function osu(client, message, args) {
+    try {
+        if (args.length == 0) return await message.reply("Por favor, especifica un nombre de usuario de osu! D:");
+
+        const username = args.join("%20");
+
+        const channel = await client.channels.get(message.channel.id);
+        const hex = randomColor().replace("#", "");
+        return channel.sendMessage(`http://lemmmy.pw/osusig/sig.php?colour=hex${hex}&uname=${username}&mode=3&pp=1&countryrank`);
+
+    } catch (err) {
+        console.log(err);
+    }
+
 });
 // 
 client.login(process.env.BOT_TOKEN);
