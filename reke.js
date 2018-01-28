e47da95d699af398ae35d9376329de0420edbaa9
Rekete <35660663+Rekete@users.noreply.github.com>
2018-01-28 12:01:00 +0100

    Update reke.js

--- reke.js
+++ reke.js
@@ -149,7 +149,9 @@ client.on("message", async message => {
 
     return message.reply(`**Stars:** ${stars} | **OD:** ${od} | **Objectos:** ${utils.addCommas(objectCount)} | **PP:** ${utils.addCommas((Math.floor(pp * 100) / 100))}`);
 
-}
+} catch (err) {
+        console.log(err);
+    }}
     
     if(command === "osu") {
         
